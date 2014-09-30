0262746350ffd1787fa66a341c661e346d31f741
jennifermccarthy <student@epicodus.com>
2014-09-30 10:12:32 -0700

    complete devise authentication, validate user_id on a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
 
   validates :content, presence: true
+  validates :user_id, presence: true
 
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Post do
   it { should validate_presence_of :content }
+  it { should validate_presence_of :user_id }
 end
